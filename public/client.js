65ea43a0b4fe66f5314213c123fc7a2d91ae3b0a
Glitch (hello-express) <none>
2019-08-10 04:22:13 +0000

    🎅🏅 Checkpoint ./public/client.js:867163/1992 ./README.md:867163/1020

--- public/client.js
+++ public/client.js
@@ -5,15 +5,10 @@ const form = document.querySelector('.searchForm');
 form.addEventListener('submit', handleSubmit);
 
 function handleSubmit(event) {
-    // prevent page from reloading when form is submitted
   event.preventDefault();
-  // get the value of the input field
   const input = document.querySelector('.search-input').value;
-  // remove whitespace from the input
   const searchQuery = input.trim();
-  // call `fetchResults` and pass it the `searchQuery`
   fetchResults(searchQuery);
-
 }
 
 function fetchImage(searchQuery){
@@ -24,7 +19,7 @@ function fetchImage(searchQuery){
         const result = data.query.pages;
         console.log('result', result);
         const id = Object.keys(result)[0];
-        if(id !== -1){
+        if(result[id].original){
           const imgURL = result[id].original.source;
           console.log(imgURL); 
           displayImage(imgURL);
@@ -51,64 +46,18 @@ function displayImage(imageURL){
 }
 
 function displayResults(results) {
-  // Store a reference to `.searchResults`
   const searchResults = document.querySelector('.searchResults');
-  // Remove all child elements
   searchResults.innerHTML = '';
-  // Loop over results array
-    const result = results[0];
-    const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
+  const result = results[0];
+  const url = encodeURI(`https://en.wikipedia.org/wiki/${result.title}`);
 
-     searchResults.insertAdjacentHTML('beforeend',
-      `<div class="resultItem">
-        <h3 class="resultItem-title">
-          <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
-        </h3>
-        <span class="resultItem-snippet">${result.snippet}</span><br>
-      </div>`
-    );
+   searchResults.insertAdjacentHTML('beforeend',
+    `<div class="resultItem">
+      <h3 class="resultItem-title">
+        <a href="${url}" target="_blank" rel="noopener">${result.title}</a>
+      </h3>
+      <span class="resultItem-snippet">${result.snippet}</span><br>
+    </div>`
+  );
 }
-
-const saveSearch = (searchQuery) => {
   
-}
-
-
-
-// // our default array of dreams
-// const dreams = [
-//   'Find and count some sheep',
-//   'Climb a really tall mountain',
-//   'Wash the dishes'
-// ];
-
-// // define variables that reference elements on our page
-// const dreamsList = document.getElementById('dreams');
-// const dreamsForm = document.forms[0];
-// const dreamInput = dreamsForm.elements['dream'];
-
-// // a helper function that creates a list item for a given dream
-// const appendNewDream = function(dream) {
-//   const newListItem = document.createElement('li');
-//   newListItem.innerHTML = dream;
-//   dreamsList.appendChild(newListItem);
-// }
-
-// // iterate through every dream and add it to our page
-// dreams.forEach( function(dream) {
-//   appendNewDream(dream);
-// });
-
-// // listen for the form to be submitted and add a new dream when it is
-// dreamsForm.onsubmit = function(event) {
-//   // stop our form submission from refreshing the page
-//   event.preventDefault();
-
-//   // get dream value and add it to the list
-//   dreams.push(dreamInput.value);
-//   appendNewDream(dreamInput.value);
-
-//   // reset form 
-//   dreamInput.value = '';
-//   dreamInput.focus();
-// };
